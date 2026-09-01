ace72d2dfa133562be896a977ee97ce3dd72d9a0
inkerd87 <inkerdany@email.com>
2026-09-01 16:56:51 +0300

    Fix Passenger port socket handling and add app.js entry point

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "0.0.0.0";
-const port = parseInt(process.env.PORT || "3000", 10);
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
-  createServer(async (req, res) => {
+  const server = createServer(async (req, res) => {
     try {
       const parsedUrl = parse(req.url, true);
       await handle(req, res, parsedUrl);
@@ -18,8 +18,10 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end("internal server error");
     }
-  }).listen(port, (err) => {
+  });
+
+  server.listen(port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on port ${port}`);
   });
 });
